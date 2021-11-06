d1f2b0e500a8569b0ed0bedd0f0f2671b90cf2fc
Francisco Rafart <francisco.rafart@gmail.com>
2021-11-05 20:44:01 -0400

    Embed pyplot inside Tkinter

--- code/audio_context.py
+++ code/audio_context.py
@@ -1,23 +1,40 @@
-from audio_utils import play_audio_file
-from main import process_file
+import numpy as np
+from audio_utils import play_audio_file, write_audio_file, shift_phase,read_audio_file
 from tkinter import filedialog
+from effects import apply_pedal
+
+processed_filename = 'audio/processed.wav'
+null_test_filename = 'audio/null-test.wav'
+
+def process_file(filename):
+    # Original signal, processed signal, and null test signal
+    fs, signal = read_audio_file(filename)
+    processed_signal = apply_pedal(signal, fs)
+    x_shifted = shift_phase(signal, np.pi) # Phase shifting x by 180 degrees (π)
+
+    min_samples = min(x_shifted.shape[0], processed_signal.shape[0])
+    # Combine the two signals to extract pure effect
+    null_signal = processed_signal[0:min_samples] + x_shifted[0:min_samples]
+
+    write_audio_file(null_signal, fs, null_test_filename)
+    write_audio_file(processed_signal, fs, processed_filename)
+
+    return signal, processed_signal, null_signal, min_samples, fs
 
 class AudioContext():
     def __init__(self):
         self.original_filename = None
-        self.processed_filename = None
-        self.null_test_filename = None
+        self.processed_filename = processed_filename
+        self.null_filename = null_test_filename
 
-    # Move to its own folder
     def import_click(self):
         self.original_filename = filedialog.askopenfilename(filetypes = (("Wav files","*.wav"),("all files","*.*")))
-        null, processed = process_file(self.original_filename)
+        signal, processed_signal, null_signal, min_samples, fs = process_file(self.original_filename)
 
-        self.null_test_filename = null
-        self.processed_filename = processed
+        return signal, processed_signal, null_signal, min_samples, fs
 
     def play_null(self):
-        play_audio_file(self.null_test_filename)
+        play_audio_file(self.null_filename)
 
     def play_processed(self):
         play_audio_file(self.processed_filename)
--- code/interface.py
+++ code/interface.py
@@ -1,25 +1,70 @@
 from tkinter import Label, Button, Tk
 from audio_context import AudioContext
+# from plot import Plot
+import numpy as np
+import matplotlib.pyplot as plt
+from audio_utils import spectrum
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 ctx = AudioContext()
-
 window = Tk()
+
+def click():
+    signal, processed_signal, null_signal, min_samples, fs = ctx.import_click()
+    fig, ((x1, k1), (x2, k2), (x3, k3)) = plt.subplots(3, 2)
+
+    # NOTE: To display samples in seconds
+    tx = np.arange(signal.shape[0]) / float(fs)
+    t_processedx = np.arange(min_samples) / float(fs)
+
+    fig.suptitle('Signals')
+
+    x1.set_ylim([-1,1])
+    x2.set_ylim([-1,1])
+    x3.set_ylim([-1,1])
+
+    x1.plot(tx, signal, label='original signal')
+    x2.plot(t_processedx, processed_signal, color='g', label='processed signal')
+    x3.plot(t_processedx, null_signal, color='r', label='null test')
+
+    # TODO: Plot spectrums with logarithmic scale
+    tk = np.arange(12000)
+    # k1.set_xscale('log')
+    k1.plot(tk, spectrum(signal)[0:12000])
+    # k2.xscale('log')
+    k2.plot(tk, spectrum(processed_signal)[0:12000], color='g')
+    # k3.xscale('log')
+    k3.plot(tk, spectrum(null_signal)[0:12000], color='r')
+
+    canvas = FigureCanvasTkAgg(fig,
+                               master = window)  
+    canvas.draw()
+  
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,
+                                   window)
+    toolbar.update()
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
+
 window.title('Null Test Application')
 
 label = Label(window, text='Import an audio file and apply processing', font=('Arial', 20))
-label.grid(column=0, row=0)
 
-import_btn = Button(window, text='Import audio', fg='green', command=ctx.import_click)
-import_btn.grid(column=0, row=1)
+import_btn = Button(window, text='Import audio', command=click)
+import_btn.pack()
 
-play_null_button = Button(window, text='Play null', command=ctx.play_null)
-play_null_button.grid(column=0, row=2)
+play_original_btn = Button(window, text='Play original', fg='blue', command=ctx.play_original)
+play_original_btn.pack()
 
-play_processed_button = Button(window, text='Play processed', command=ctx.play_processed)
-play_processed_button.grid(column=0, row=3)
+play_processed_button = Button(window, text='Play processed', fg='green', command=ctx.play_processed)
+play_processed_button.pack()
 
-play_original_btn = Button(window, text='Play original', command=ctx.play_original)
-play_original_btn.grid(column=0, row=4)
+play_null_button = Button(window, text='Play null', fg='red', command=ctx.play_null)
+play_null_button.pack()
 
 # window size
 window.geometry('800x600')
