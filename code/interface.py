3cd0012d19a7f6b5b1393bafa8023945ddfb5a79
Francisco Rafart <francisco.rafart@gmail.com>
2021-11-09 15:00:01 -0500

    Improve UI layout

--- code/interface.py
+++ code/interface.py
@@ -1,4 +1,5 @@
 from tkinter import Label, Button, StringVar, Tk, OptionMenu, Entry, DoubleVar, IntVar
+from tkinter.constants import ANCHOR
 from audio_context import AudioContext
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,11 +21,11 @@ class Interface():
         # window size
         self.window.geometry('1200x900')
 
-        self.label = Label(self.window, text='Import an audio file and apply processing', font=('Arial', 20))
-        self.label.pack()
-
         self.import_btn = Button(self.window, text='Import audio', command=self.click)
-        self.import_btn.pack()
+        self.import_btn.grid(row=0, column=0)
+
+        self.process_btn = Button(self.window, text='Process Audio', command=self.process, state='disabled')
+        self.process_btn.grid(row=0, column=1)
 
         self.dropdown = None
         self.menu = None
@@ -32,30 +33,44 @@ class Interface():
         self.figure = None
 
         # None buttons
-        self.process_btn = None
-
         self.play_original_btn = None
         self.play_processed_button = None
         self.play_null_button = None
 
         self.effects = [] # {effect_key: xxx, config_params: [(param_key, Label, Entry)]}
+        self.new_effect_row = 3
 
 
     def mainloop(self):
         self.window.mainloop()
 
-    def click(self):
+    def reset_uid_and_effects(self):
         if self.process_btn:
-            self.process_btn.destroy()
+            self.process_btn.config(state='disabled')
+
+        self.remove_play_buttons()
+
+        # Remove effects UI
+        for effect in self.effects:
+            effect['effect_label'].destroy()
+
+            for (_, label, entry) in effect['config_params']:
+                label.destroy()
+                entry.destroy()
+
+        self.effects = []
+        if self.dropdown:
+            self.dropdown.destroy()
+
+        self.pedal.clear_effects()
 
         # TODO: Figure out clear matplotlib
         if self.figure:
             self.figure.clf()
 
-        self.remove_play_buttons()
 
-        self.pedal.clear_effects()
-
+    def click(self):
+        self.reset_uid_and_effects()
         self.ctx.import_click()
 
         #Set the Menu initially
@@ -64,10 +79,10 @@ class Interface():
 
         self.menu = menu
         self.dropdown = OptionMenu(self.window, menu , *effect_map.keys())
-        self.dropdown.pack()
+        self.dropdown.grid(row=1, column=0)
 
         self.add_effect = Button(self.window, text='+', command=self.add_effect)
-        self.add_effect.pack()
+        self.add_effect.grid(row=1, column=1)
 
     def add_effect(self): # config
         effect_identifier = self.menu.get()
@@ -76,21 +91,26 @@ class Interface():
         effect_class = effect_dict['instance']
         default_config = effect_dict['config']
 
-        effect_label = Label(self.window, text='{}. {}'.format(len(self.effects) + 1, effect_identifier))
-        effect_label.pack()
+        effect_label = Label(self.window, text='- {}'.format(effect_identifier.capitalize()), font=14)
+        effect_label.grid(row=self.new_effect_row)
 
         config_params = []
         for param_key, v in default_config.items():
+            param_index = len(config_params)
             default_value = DoubleVar()
             default_value.set(v)
 
             label = Label(self.window, text=param_key)
             param_entry = Entry(self.window, textvariable=default_value)
 
-            label.pack(side='left')
-            param_entry.pack(side='left')
+            label.grid(row=self.new_effect_row + 1, column=param_index*2)
+            param_entry.grid(row=self.new_effect_row + 1, column=1 + (param_index * 2))
+
+            # Something is worng here
 
             config_params.append((param_key, label, param_entry))
+
+        self.new_effect_row = self.new_effect_row + 2
 
         self.effects.append({
             'effect': effect_class,
@@ -98,29 +118,30 @@ class Interface():
             'config_params': config_params,
         })
 
-        # Add process button when first effect added only
-        if len(self.effects) == 1 and not self.process_btn:
-            self.process_btn = Button(self.window, text='Process Audio', command=self.process)
-            self.process_btn.pack()
+        # Enable process button when first effect added only
+        if len(self.effects) >= 1:
+            self.process_btn.config(state="active")
 
     def set_effect(self):
         for effect in self.effects:
             effect_config = {}
             for (param_key, _, param_entry) in effect['config_params']: 
-                effect_config[param_key] = float(param_entry.get())
+                effect_config[param_key] = float(param_entry.get()) # TODO: Fix string issues for filter. Use is_number function to decide which type
 
             self.pedal.add_effect(effect['effect'], effect_config)
 
 
     def add_play_buttons(self):
         self.play_original_btn = Button(self.window, text='Play original', fg='blue', command=self.ctx.play_original)
-        self.play_original_btn.pack()
+        self.play_original_btn.grid(row=self.new_effect_row, column=0)
 
         self.play_processed_button = Button(self.window, text='Play processed', fg='green', command=self.ctx.play_processed)
-        self.play_processed_button.pack()
+        self.play_processed_button.grid(row=self.new_effect_row, column=1)
 
         self.play_null_button = Button(self.window, text='Play null', fg='red', command=self.ctx.play_null)
-        self.play_null_button.pack()
+        self.play_null_button.grid(row=self.new_effect_row, column=2)
+
+        self.new_effect_row = self.new_effect_row + 1
 
     def remove_play_buttons(self):
         if self.play_original_btn:
@@ -165,13 +186,15 @@ class Interface():
         # NOTE: Add pyplot to tkinter reference: https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
         canvas = FigureCanvasTkAgg(fig, master = self.window)  
         canvas.draw()
+
         # placing the canvas on the Tkinter window
-        canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().place(relx=0.5, rely=1, anchor='s')
+
         # creating the Matplotlib toolbar
-        toolbar = NavigationToolbar2Tk(canvas, self.window)
+        toolbar = NavigationToolbar2Tk(canvas, self.window)  # TODO: Breaking here!
         toolbar.update()
         # placing the toolbar on the Tkinter window
-        canvas.get_tk_widget().pack()
+        # canvas.get_tk_widget().pack()
 
 interface = Interface()
 interface.mainloop()
