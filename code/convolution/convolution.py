02d2f313796bb237c6a29c5e32ca2d6fb5b264c1
Francisco Rafart <francisco.rafart@gmail.com>
2021-12-02 13:59:38 -0500

    Display plots for convolution and original signals

--- code/convolution/convolution.py
+++ code/convolution/convolution.py
@@ -12,6 +12,9 @@ class Convolve():
 
         self.x = x
         self.y = y
+        self.z = None
+
+
         self.fs = fs_x
 
 
@@ -22,32 +25,44 @@ class Convolve():
 
     def set_complex_spectrum(self, gain_factor=0.1):
         x_pad, y_pad = self._zero_pad(gain_factor)
-        self.spectrum_y = complex_spectrum(y_pad)
-        self.spectrum_x = complex_spectrum(x_pad)
+        self.complex_spectrum_y = complex_spectrum(y_pad)
+        self.complex_spectrum_x = complex_spectrum(x_pad)
 
     # NOTE: Doing convolution by doing fft -> multiplying the frequency domains -> doing inverse fft. O(n log(n))
     def convolve_freq_complex(self):
         self.set_complex_spectrum()
-        product = self.spectrum_y * self.spectrum_x
 
-        return fftpack.ifft(product)
+        self.complex_spectrum_z = self.complex_spectrum_y * self.complex_spectrum_x
+        self.complex_z = fftpack.ifft(self.complex_spectrum_z)
+
+        self.z = np.real(self.complex_z)
+
+        return self.complex_z
 
     # NOTE: Doing convolution by doing fft -> multiplying the frequency domains -> doing inverse fft. O(n log(n))
     # Doing it only in the real spectrum (cuts the audio duration)
     def convolve_freq_real(self):
         self.set_real_spectrum()
-
         convolution = self.spectrum_y * self.spectrum_x
+        self.z = fftpack.irfft(convolution)
 
-        return fftpack.irfft(convolution)
+        return self.z
+
+    def get_real_spectrums(self):
+        if self.z is None:
+            return spectrum(self.x), spectrum(self.y), np.array([])
+
+        return spectrum(self.x), spectrum(self.y), spectrum(self.z)
+
+
 
     # NOTE: Direct convolution in the time domain - O(N^2)
     def convolve_time_domain(self, gain_factor=0.1):
         max_length = max(self.x.shape[0], self.y.shape[0])
         x_pad = np.concatenate([self.x, np.repeat(0, int(max_length-self.x.shape[0]))]) * gain_factor
         y_pad = np.concatenate([self.y, np.repeat(0, int(max_length-self.y.shape[0]))]) * gain_factor
-
-        return np.convolve(x_pad, y_pad)
+        self.z = np.convolve(x_pad, y_pad)
+        return self.z
 
     # Jeff Wang's implementation of convolution algorithm O(N^2)
     def convolve(x, y):
--- code/convolution/convolution_ui.py
+++ code/convolution/convolution_ui.py
@@ -1,6 +1,6 @@
 from tkinter import Button, StringVar, Tk, ttk, Frame
 from convolution.convolution import Convolve
-from audio_utils import play_audio_file, import_file
+from audio_utils import play_audio_file, import_file, spectrum
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,6 +15,7 @@ class ConvolutionUI():
 
         self.import_btn2 = Button(self.tab, text='Import Audio 2', command=lambda: self.import_file(2), width=8)
         self.import_btn2.grid(row=0, column=1)
+        self.convolved = None
 
         self.filename1 = None
         self.filename2 = None
@@ -34,11 +35,12 @@ class ConvolutionUI():
             self.add_effect_btn.grid(row=1, column=1)
 
     def convolve(self):
-        c = Convolve(self.filename1, self.filename2)
-        x = c.convolve_freq_complex()
+        self.convolved = Convolve(self.filename1, self.filename2)
+        x = self.convolved.convolve_freq_complex()
 
-        write_audio_file(x, c.fs, self.convolve_file)
+        write_audio_file(x, self.convolved.fs, self.convolve_file)
         self.add_play_buttons()
+        self.process_audio_and_plot()
 
     def add_play_buttons(self):
         self.play_original_btn = Button(self.tab, text='Play audio 1', fg='blue', command=lambda: play_audio_file(self.filename1))
@@ -52,45 +54,37 @@ class ConvolutionUI():
 
     # TODO: 1. Add wave visualization / 2. Add Entry to determine export file name
 
-    # def process_audio_and_plot(self):
+    def process_audio_and_plot(self):
+        fig, ((x1, k1), (x2, k2), (x3, k3)) = plt.subplots(3, 2)
 
-    #     # self.remove_play_buttons()
-    #     # self.add_play_buttons()
-    #     fig, ((x1, k1), (x2, k2), (x3, k3)) = plt.subplots(3, 2)
+        self.figure = fig
 
-    #     self.figure = fig
+        tx = np.arange(self.convolved.x.shape[0]) / float(self.convolved.fs) # display samples in seconds
+        ty = np.arange(self.convolved.y.shape[0]) / float(self.convolved.fs) # display samples in seconds
+        tz = np.arange(self.convolved.z.shape[0]) / float(self.convolved.fs) # display samples in seconds
 
-    #     tx = np.arange(self.ctx.signal.shape[0]) / float(self.ctx.sample_rate) # display samples in seconds
-    #     # t_processedx = np.arange(self.ctx.processed_signal.shape[0]) / float(self.ctx.sample_rate)
+        # t_processedx = np.arange(self.ctx.processed_signal.shape[0]) / float(self.ctx.sample_rate)
+        self.figure.suptitle('Convolution {} and {}'.format(self.filename1.split('/')[-1], self.filename2.split('/')[-1]))
 
-    #     self.figure.suptitle('Signals for {}'.format(self.filename.split('/')[-1]))
+        x1.set_ylim([-1,1])
+        x2.set_ylim([-1,1])
+        x3.set_ylim([-1,1])
 
-    #     x1.set_ylim([-1,1])
-    #     x2.set_ylim([-1,1])
-    #     x3.set_ylim([-1,1])
+        x1.plot(tx, self.convolved.x, label='Audio 1')
+        x2.plot(ty, self.convolved.y, color='g', label='Audio 2')
+        x3.plot(tz, self.convolved.z, color='r', label='Convolved audio')
 
-    #     x1.plot(tx, self.ctx.signal, label='Audio 1')
-    #     x2.plot(t_processedx, self.ctx.processed_signal, color='g', label='Audio 2')
-    #     x3.plot(t_processedx, self.ctx.null_signal, color='r', label='Convolved audio')
+        # tk = np.arange(12000)
+        xk, yk, zk = self.convolved.get_real_spectrums()
 
-    #     tk = np.arange(12000)
-    #     # k1.set_xscale('log')
-    #     # k1.plot(tk, self.ctx.signal_spectrum[0:12000])
-    #     # # k2.xscale('log')
-    #     # k2.plot(tk, self.ctx.processed_spectrum[0:12000], color='g')
-    #     # # k3.xscale('log')
-    #     # k3.plot(tk, self.ctx.null_spectrum[0:12000], color='r')
+        k1.plot(xk[0:12000])
+        k2.plot(yk[0:12000], color='g')
+        k3.plot(zk[0:12000], color='r')
 
-    #     # NOTE: Add pyplot to tkinter reference: https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
-    #     canvas = FigureCanvasTkAgg(fig, master = self.tab)
-    #     self.canvas = canvas
-    #     self.canvas.draw()
+        # NOTE: Add pyplot to tkinter reference: https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
+        canvas = FigureCanvasTkAgg(fig, master = self.tab)
+        self.canvas = canvas
+        self.canvas.draw()
 
-    #     # placing the canvas on the Tkinter window
-    #     self.canvas.get_tk_widget().place(relx=0.5, rely=1, anchor='s')
-
-    #     # creating the Matplotlib toolbar
-    #     # toolbar = NavigationToolbar2Tk(self.canvas, self.tab)  # TODO: Breaking here!
-    #     # toolbar.update()
-    #     # placing the toolbar on the Tkinter window
-    #     # canvas.get_tk_widget().pack()
+        # placing the canvas on the Tkinter window
+        self.canvas.get_tk_widget().place(relx=0.5, rely=1, anchor='s')
